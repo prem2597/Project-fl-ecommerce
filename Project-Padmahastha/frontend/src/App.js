97f1ba40bf4b21a6457bbe867256189b5c39b6e6
Prem Kumar <prembammidi25@msitprogram.net>
2020-05-20 12:16:01 +0530

    product details

--- Project-Padmahastha/frontend/src/App.js
+++ Project-Padmahastha/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import data from  './data';
 
 function App() {
 
@@ -44,72 +45,21 @@ const closeMenu = () => {
       <main className="main">
         <div className="content">
           <ul className="products">
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/d1.jpg" alt="product"></img>
-                <div className="product-name">
-                  <a href="product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/d1.jpg" alt="product"></img>
-                <div className="product-name">
-                  <a href="product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/d1.jpg" alt="product"></img>
-                <div className="product-name">
-                  <a href="product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/d1.jpg" alt="product"></img>
-                <div className="product-name">
-                  <a href="product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/d1.jpg" alt="product"></img>
-                <div className="product-name">
-                  <a href="product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/d1.jpg" alt="product"></img>
-                <div className="product-name">
-                  <a href="product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
+            {
+              data.products.map(product =>
+                <li>
+                  <div className="product">
+                    <img className="product-image" src={product.image} alt="product"></img>
+                    <div className="product-name">
+                      <a href="product.html">{product.name}</a>
+                    </div>
+                    <div className="product-brand">{product.brand}</div>
+                    <div className="product-price">{product.price}</div>
+                    <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
+                  </div>
+                </li>
+              )
+            }
           </ul>
         </div>
       </main>
---
+++ Project-Padmahastha/frontend/src/data.js
@@ -0,0 +1,40 @@
+export default {
+    products : [
+        {
+            name: 'Slim shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+        },
+        {
+            name: 'Fit shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 10,
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+        },
+    ] 
+}
