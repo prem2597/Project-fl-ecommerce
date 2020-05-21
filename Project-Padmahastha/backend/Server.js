f1b597f1c8d70bb5d612a46f780e6bc15336e365
Prem Kumar <prembammidi25@msitprogram.net>
2020-05-21 14:55:58 +0530

    Api call setup

--- Project-Padmahastha/backend/Server.js
+++ Project-Padmahastha/backend/Server.js
@@ -3,9 +3,8 @@ import data from './data';
 
 const app = express();
 
-app.get("/api/products",(req, res) => {
+app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
-app.listen(5000, () => {console.log("Server started at http://localhost:5000");
-})
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
--- Project-Padmahastha/frontend/src/App.js
+++ Project-Padmahastha/frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-// import logo from './logo.svg';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
-// import data from  './data';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
 
