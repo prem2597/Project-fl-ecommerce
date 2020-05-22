263ec6885890af4f34861ebf5e02e2d53360b513
dharshakch97-cloud <saidh1971>
2020-05-22 12:01:06 +0530

    React initial

---
+++ E-commerce project/frontend/src/data.js
@@ -0,0 +1,34 @@
+export default {
+    products: [
+        {
+            _id : 1,
+            name : 'Slim shirt',
+            category : 'Shirts',
+            image : '/images/d1.jpg',
+            price : 60,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 10
+        },
+        {
+            _id : 2,
+            name : 'Fit shirt',
+            category : 'Shirts',
+            image : '/images/d1.jpg',
+            price : 60,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 10
+        },
+        {
+            _id : 3, 
+            name : 'Best pants',
+            category : 'Pants',
+            image : '/images/d1.jpg',
+            price : 70,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 6
+        },
+    ]
+}
