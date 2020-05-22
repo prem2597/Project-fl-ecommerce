c778708a8c4ec556ddb1d90f4a1dd79516db1607
Prem Kumar <prembammidi25@msitprogram.net>
2020-05-22 12:34:41 +0530

    Cart Page checkout option

--- Project-Padmahastha/frontend/src/App.js
+++ Project-Padmahastha/frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
 
@@ -48,6 +49,7 @@ const closeMenu = () => {
         <main className="main">
           <div className="content">
             <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
             <Route path="/" exact={true} component={HomeScreen} />
           </div>
         </main>
