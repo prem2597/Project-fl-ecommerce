f0baf4946fbb64ef6b65ef0d25a88f4b25db632d
unknown <Srii1020>
2020-05-22 18:55:53 +0530

    product details from backend

---
+++ Padmahastha/backend/data.js
@@ -0,0 +1,49 @@
+export default {
+    products:[
+     
+        {
+            _id:'1',
+            name:"Slim  shirt",
+            category:"shirts",
+            image:"/Images/d1.jpg",
+            price:60,
+            brand: "Nike",
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'2',
+            name:"Fit  shirt",
+            category:"shirts",
+            image:"/Images/d1.jpg",
+            price:80,
+            brand:"Nike",
+            rating:4.3,
+            numReviews:10
+        },
+        {
+            _id:'3',
+            name:"Best pant",
+            category:"pants",
+            image:"/Images/d1.jpg",
+            price:90,
+            brand:"Nike",
+            rating:4.2,
+            numReviews:10
+        },
+        {
+            _id:'4',
+            name:"Fit shirt",
+            category:"shirts",
+            image:"/Images/d1.jpg",
+            price:80,
+            brand:"Nike",
+            rating:4.3,
+            numReviews:10
+        },
+       
+        
+
+    ]
+
+}
