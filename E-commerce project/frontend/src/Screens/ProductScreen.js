848602da53769818bbde1a9912800d6e83a9c0f1
dharshakch97-cloud <saidh1971>
2020-05-22 16:47:13 +0530

    can't resolve the issue (product details)

--- E-commerce project/frontend/src/Screens/ProductScreen.js
+++ E-commerce project/frontend/src/Screens/ProductScreen.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import data from '../data';
+import { Link } from 'react-router-dom';
 
 function ProductScreen(props) {
 	console.log(props.match.params.id);
 	const product = data.products.find(x => x._id === props.match.params.id);
 	// console.log(product.name);
   	return <div>
-	  	<h1>{product.name}</h1>
+		<div><Link to="/">Back to homepage</Link></div>
+		<div className="details">
+			<div className="details-image">
+				<img src={product.image} alt="product" />
+			</div>
+			<div className="details-info">
+				<ul>
+					<li>
+						<h4>{product.name}</h4>
+					</li>
+					<li>
+						{product.rating} Stars ({product.numReviews} Reviews)
+					</li>
+					<li>
+						<b>{product.price}</b>
+					</li>
+					<li>
+						Description:
+						<div>{product.description}</div>
+					</li>
+				</ul>
+			</div>
+		</div>
   	</div>
 }
 
