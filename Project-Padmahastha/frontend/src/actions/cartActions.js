5bb7abd9c0971170c0fc65c4c70a514f1730d937
Prem Kumar <prembammidi25@msitprogram.net>
2020-05-22 13:33:15 +0530

    Cart delete option qty set

--- Project-Padmahastha/frontend/src/actions/cartActions.js
+++ Project-Padmahastha/frontend/src/actions/cartActions.js
@@ -1,5 +1,6 @@
 import Axios from "axios"
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import Cookie from "js-cookie";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 const addToCart = (productId, qty) => async (dispatch, getState) => {
     try {
@@ -11,10 +12,19 @@ const addToCart = (productId, qty) => async (dispatch, getState) => {
             price: data.price,
             countInStock: data.countInStock,
             qty
-        }})
+        }
+    });
+    const {cart:{cartItems}} = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
     } catch (error) {
         
     }
 }
 
-export { addToCart }
+const removeFromCart = (productId) => (dispatch, getState) => {
+    dispatch({type: CART_REMOVE_ITEM, payload: productId });
+    const {cart:{cartItems}} = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
+}
+
+export { addToCart, removeFromCart }
--- Project-Padmahastha/frontend/src/screens/CartScreen.js
+++ Project-Padmahastha/frontend/src/screens/CartScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function CartScreen(props) {
 
@@ -11,12 +12,19 @@ function CartScreen(props) {
     const productId = props.match.params.id;
     const qty = props.location.search ? Number(props.location.search.split("=")[1]):1;
     const dispatch = useDispatch();
+    const removeFromCartHandler = (productId) => {
+        dispatch(removeFromCart(productId));
+    }
 
     useEffect(() => {
         if (productId) {
             dispatch(addToCart(productId, qty));
         }
-    }, [dispatch, productId, qty])
+    }, [dispatch, productId, qty]);
+
+    const checkoutHandler = () => {
+        props.history.push("/signin?redirect=shipping");
+    }
 
     return <div className="cart">
         <div className="cart-list">
@@ -36,25 +44,32 @@ function CartScreen(props) {
                     </div>
                     :
                     cartItems.map(item =>
-                        <div>
-                            <img src={item.image} alt="product" />
+                        <li>
+                            <div className="cart-image">
+                                <img src={item.image} alt="product" />
+                            </div>
                             <div className="cart-name">
                                 <div>
-                                    {item.name}
+                                    <Link to={"/product/" + item.product}>
+                                        {item.name}
+                                    </Link>
                                 </div>
                                 <div>
                                     Qty:
-                                    <select>
+                                    <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product, e.target.value))}>
                                         <option valu="1">1</option>
                                         <option valu="2">2</option>
                                         <option valu="3">3</option>
                                     </select>
+                                    <button type="button" className="button" onClick={ () => removeFromCartHandler(item.product)}>
+                                        Delete
+                                    </button>
                                 </div>
                             </div>
-                            <div>
-                                {item.price}
+                            <div className="cart-price">
+                                ${item.price}
                             </div>
-                        </div>
+                        </li>
                     )
                 }
             </ul>
@@ -65,7 +80,7 @@ function CartScreen(props) {
                 :
                 $ {cartItems.reduce((a,c) => a + c.price * c.qty, 0)}
             </h3>
-            <button className="button primary" disabled={cartItems.length === 0}>
+            <button onClick={checkoutHandler} className="button primary full-width" disabled={cartItems.length === 0}>
                 Proceed to checkout
             </button>
         </div>
