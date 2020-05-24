8f4188d0898433b4cc70559787c2ae02621c40fb
Prem Kumar <prembammidi25@msitprogram.net>
2020-05-24 12:10:42 +0530

    signin page backend setup

--- Project-Padmahastha/backend/util.js
+++ Project-Padmahastha/backend/util.js
@@ -1,5 +1,12 @@
 import jwt from 'jsonwebtoken';
+import config from './config';
 
 const getToken = (user) => {
-    return jwt.sign(user, config.JWT_SECRET)
+    return jwt.sign(user, config.JWT_SECRET, {
+        expiresIn: '48h'
+    })
+}
+
+export {
+    getToken
 }
