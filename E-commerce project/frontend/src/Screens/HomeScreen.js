7317adaae7bec2f84949d6c14324c7afe05c668b
dharshakch97-cloud <saidh1971>
2020-05-25 12:12:13 +0530

    sorted the issue

--- E-commerce project/frontend/src/Screens/HomeScreen.js
+++ E-commerce project/frontend/src/Screens/HomeScreen.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import data from '../data';
 
 function HomeScreen(props) {
   return <ul className="products">
-      {
-        data.products.map(product =>
-          <li key={product._id}>
-            <div className="product">
-                <Link to={'/product/' + product._id}>
-                    <img className="product-image" src={product.image} alt="product1"/>
-                </Link>
-                <div className="product-name">
-                    <Link to={'/product/' + product._id}>{product.name}</Link>
-                </div>
-                <div className="product-brand">{product.brand}</div>
-                <div className="product-price">{product.price}</div>
-                <div className="product-rating">{product.rating} Stars {product.numReviews}</div>
+    {
+      data.products.map(product =>
+        <li key={product._id}>
+          <div className="product">
+            <Link to={'/product/' + product._id}>
+              <img className="product-image" src={product.image} alt="product" />
+
+            </Link>
+            <div className="product-name">
+              <Link to={'/product/' + product._id}>{product.name}</Link>
             </div>
-          </li>)
-      }
+            <div className="product-brand">{product.brand}</div>
+            <div className="product-price">${product.price}</div>
+            <div className="product-rating">{product.rating} Stars ({product.numReiews} Reviews)</div>
+          </div>
+        </li>)
+    }
   </ul>
 }
-
 export default HomeScreen;
