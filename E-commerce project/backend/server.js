abc3fda37270df19163155876074c9a2c157ab61
dharshakch97-cloud <saidh1971>
2020-05-30 11:02:57 +0530

    connected mongodb

--- E-commerce project/backend/server.js
+++ E-commerce project/backend/server.js
@@ -1,7 +1,23 @@
 import express from 'express';
 import data from './data';
+import config from './config';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+
 const app = express();
 
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
     const product = data.products.find(x => x._id === productId);
@@ -14,4 +30,5 @@ app.get("/api/products/:id", (req, res) => {
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
+
 app.listen(5000, () => {console.log("Server started at http://localhost:5000") });
