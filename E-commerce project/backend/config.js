7a63a92a20e8b22f7ef7112d8456865ad0bd5f8d
dharshakch97-cloud <saidh1971>
2020-05-30 11:15:37 +0530

    added signin route

--- E-commerce project/backend/config.js
+++ E-commerce project/backend/config.js
@@ -1,3 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/ecom'
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/ecom',
+    JWT_SECRET: process.env.JWT_SECRET || 'smsecret'
 }
--- E-commerce project/backend/routes/userRoute.js
+++ E-commerce project/backend/routes/userRoute.js
@@ -3,6 +3,27 @@ import User from '../models/userModel';
 
 const router = express.Router();
 
+router.post("/signin", async(req, res) => {
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    if (signinUser) {
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin:signinUser.isAdmin,
+            token: getToken(user)
+        })
+    }
+    else {
+        res.status(401).send({ msg: "Invalid Email or Password." });
+    }
+})
+
+
 router.get("/admin", async (req, res) => {
   try {
     const user = new User({
