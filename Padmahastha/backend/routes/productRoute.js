c622aee953add74c2b11b743f90b438c667c0146
unknown <Srii1020>
2020-06-02 19:38:19 +0530

    added security for changing products

--- Padmahastha/backend/routes/productRoute.js
+++ Padmahastha/backend/routes/productRoute.js
@@ -10,7 +10,7 @@ router.get("/", async(req,res) => {
     res.send(products);
 })
 
-router.post("/", async(req,res) => {
+router.post("/", isAuth,isAdmin, async(req,res) => {
     const product = new Product({
         name: req.body.name,
         price: req.body.price,
@@ -30,7 +30,7 @@ router.post("/", async(req,res) => {
     return res.status(500).send({message:"Error in Creating Product"})
 })
 
-router.put("/:id", async(req,res) => {
+router.put("/:id", isAuth,isAdmin, async(req,res) => {
     const productId = req.params.id;
     // const product = await Product.findById({productId});
     const product = await Product.findById(productId);
@@ -55,7 +55,7 @@ router.put("/:id", async(req,res) => {
     return res.status(500).send({message:"Error in Updating Product"});
 });
 
-router.delete("/:id" , async(req,res) => {
+router.delete("/:id" ,isAuth,isAdmin, async(req,res) => {
     const deletedProduct = await Product.findById(req.params.id);
     if(deletedProduct) {
         await deletedProduct.remove();
--- Padmahastha/backend/util.js
+++ Padmahastha/backend/util.js
@@ -28,11 +28,15 @@ const isAuth = (req,res,next) => {
             req.user = decode;
             next();
             return
-        })
+        });
+    }
+        else {
+            return res.status(401).send({msg:"Token is not supplied."})
+        }
     }
 
-    return res.status(401).send({msg:"Token is not supplied."})
-}
+   
+
 
 const isAdmin = (req,res,next) => {
     if(req.user && req.user.isAdmin) {
