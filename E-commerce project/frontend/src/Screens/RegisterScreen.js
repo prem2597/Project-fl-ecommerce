98ca0fe5178961edde6a692b86aa0daf3faef3f1
dharshakch97-cloud <saidh1971>
2020-06-05 22:59:27 +0530

    register and signin links updated

--- E-commerce project/frontend/src/Screens/RegisterScreen.js
+++ E-commerce project/frontend/src/Screens/RegisterScreen.js
@@ -12,10 +12,11 @@ function RegisterScreen(props) {
     const userRegister = useSelector(state => state.userRegister);
     const { loading, userInfo, error } = userRegister;
     const dispatch = useDispatch();
+    const redirect = props.location.search ? props.location.search.split("=")[1] : '/';
 
     useEffect(() => {
         if (userInfo) {
-            props.history.push("/");
+            props.history.push(redirect);
         }
         return () => {
             //
@@ -55,7 +56,9 @@ function RegisterScreen(props) {
                     </input>
                 </li>
                 <li><button type="submit" className="button primary">Register</button></li>
-                <li>Already have an account? <Link to="/signin">Sign-in</Link></li>
+                <li>Already have an account? 
+                    <Link to={redirect === "/" ? "signin" : "signin?redirect=" + redirect} className="button secondary text-center">Sign-in</Link>
+                </li>
             </ul>
         </form>
     </div>
--- E-commerce project/frontend/src/Screens/SigninScreen.js
+++ E-commerce project/frontend/src/Screens/SigninScreen.js
@@ -10,10 +10,11 @@ function SigninScreen(props) {
     const userSignin = useSelector(state => state.userSignin);
     const { loading, userInfo, error } = userSignin;
     const dispatch = useDispatch();
+    const redirect = props.location.search ? props.location.search.split("=")[1] : '/';
 
     useEffect(() => {
         if (userInfo) {
-            props.history.push("/");
+            props.history.push(redirect);
         }
         return () => {
             //
@@ -47,7 +48,7 @@ function SigninScreen(props) {
                 <li><button type="submit" className="button primary">Signin</button></li>
                 <li>New to amazona?</li>
                 <li>
-                    <Link to="/register" className="button secondary text-center" >Create your amazona account</Link>
+                    <Link to={redirect === "/" ? "register" : "register?redirect=" + redirect} className="button secondary text-center" >Create your amazona account</Link>
                 </li>
             </ul>
         </form>
