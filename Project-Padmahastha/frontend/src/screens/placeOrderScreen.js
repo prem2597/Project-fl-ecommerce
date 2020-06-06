841ee9a4b66b532063c1c40f37e7df61a7611fc8
Prem Kumar <prembammidi25@msitprogram.net>
2020-06-06 21:04:44 +0530

    place order page

--- Project-Padmahastha/frontend/src/screens/placeOrderScreen.js
+++ Project-Padmahastha/frontend/src/screens/placeOrderScreen.js
@@ -3,11 +3,14 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import CheckoutSteps from '../components/checkoutSteps';
+import { createOrder } from '../actions/orderActions';
 
 function PlaceOrderScreen(props) {
 
     const cart = useSelector(state => state.cart);
+    const orderCreate = useSelector(state => state.orderCreate);
 
+    const { loading, success, error, order } = orderCreate;
     const { cartItems, shipping, payment } = cart;
 
     if (!shipping.address) {
@@ -20,14 +23,19 @@ function PlaceOrderScreen(props) {
     const shippingPrice = itemsPrice > 100 ? 0 : 10;
     const taxPrice = 0.15 * itemsPrice;
     const totalPrice = itemsPrice + shippingPrice + taxPrice;
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
     const placeOrderHandler = () => {
-    // create an order
+        dispatch(createOrder({
+            orderItems: cartItems, shipping, payment, itemsPrice,
+            shippingPrice, taxPrice, totalPrice,
+        }))
     }
     useEffect(() => {
-
-    }, []);
+        if ( success ) {
+            props.history.push("/order/" + order._id);
+        }
+    }, [success]);
 
     const checkoutHandler = () => {
         props.history.push("/signin?redirect=shipping");
--- Project-Padmahastha/frontend/src/store.js
+++ Project-Padmahastha/frontend/src/store.js
@@ -4,6 +4,7 @@ import Cookie from 'js-cookie';
 import { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import { userSigninReducer, userRegisterReducer } from './reducers/userReducers';
+import { OrderCreateReducer } from './reducers/orderReducers';
 
 const cartItems = Cookie.getJSON("cartItems") || [];
 const userInfo = Cookie.getJSON("userInfo") || null;
@@ -17,6 +18,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     productSave: productSaveReducer,
     productDelete: productDeleteReducer,
+    orderCreate: OrderCreateReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
