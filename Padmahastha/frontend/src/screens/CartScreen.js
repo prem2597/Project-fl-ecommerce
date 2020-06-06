67659faecc350ccf01dcb7d01535bc10eb42a7b8
Manideepnetha <manideepnetha24@msitprogram.net>
2020-06-06 19:28:37 +0530

    Shopping Cart Screen (Advanced React)

--- Padmahastha/frontend/src/screens/CartScreen.js
+++ Padmahastha/frontend/src/screens/CartScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import { addToCart } from '../actions/cartActions';
+import { addToCart,removeFromCart } from '../actions/cartActions';
 import { useDispatch, useSelector } from 'react-redux';
+import{Link} from 'react-router-dom';
 function CartScreen(props){
 
     const cart = useSelector(state => state.cart);
@@ -9,11 +10,17 @@ function CartScreen(props){
     const productId = props.match.params.id;
     const qty = props.location.search? Number(props.location.search.split("=")[1]):1;
     const dispatch = useDispatch();
-    useEffect(() => {
+    const removeFromCartHandler = (productId)=> {
+    dispatch(removeFromCart(productId));
+}    
+useEffect(() => {
         if (productId){
-            dispatchEvent(addToCart(productId, qty));
+            dispatch(addToCart(productId, qty));
         }
-    }, [])
+    }, []);
+    const checkoutHandler = () =>{
+        props.history.push("/signin?redirect=shipping");
+    }
 
     return <div className="cart">
         <div className="cart-list">
@@ -34,40 +41,49 @@ function CartScreen(props){
             </div>
             :
             cartItems.map(item=>
-                <div> 
+                <li>
+                    <div className="cart-image">
                     <img src={item.image} alt="product"/>
+                        
+                    </div>
+                
                         <div className="cart-name">
                          <div> 
+                             <Link to={"/product/" + item.product}>
                              {item.name}
+                             </Link>
                          </div>
                          <div> 
                              Qty:
-                             <select>
-                                 <option value="1">1 </option>
-                                 <option value="2">2 </option>
-                                 <option value="3">3 </option>
-                                 
+                             <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product,e.target.value))}>
+                             {[...Array(item.countInStock).keys()].map(x =>
+                        <option key={x + 1} value={x + 1}>{x + 1}</option>
+                              )}
+                             
+
                                  </select>
+                                 <button type="button" classname="button" onClick={() => removeFromCartHandler(item.product)}>
+                                     Delete
+                                 </button>
                          </div>
                         </div>
-
-                        <div>
-                           {item.price}
+                         
+                        <div className="cart-price">
+                            ${item.price}
                         </div>
-                    
-                </div>
              
+             </li>
                 )
         }
         </ul>
         </div>
         <div className="cart-action">
                 <h3>
-                    Subtotal({cartItems.reduce((a,c)=>a+c.qty),0}items)
+                    Subtotal({cartItems.reduce((a,c)=>a+c.qty,0)}items)
                     :
                  ${cartItems.reduce((a,c)=> a+c.price*c.qty,0)}
                 </h3>
-                <button className ="button-primary" disabled={cartItems.length===0}>
+                <button onClick={checkoutHandler} className ="button primary full-width" disabled={cartItems.length===0}>
                     Proceed to checkout
                      </button>
                  
