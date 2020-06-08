66b6dc0210b2652fcf05f2459d44a7f74508355b
Prem Kumar <prembammidi25@msitprogram.net>
2020-06-08 22:32:45 +0530

    frontend user complete profile

--- Project-Padmahastha/frontend/src/reducers/userReducers.js
+++ Project-Padmahastha/frontend/src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_LOGOUT } from "../constants/userConstants";
+import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_LOGOUT, USER_UPDATE_REQUEST, USER_UPDATE_SUCCESS, USER_UPDATE_FAIL } from "../constants/userConstants";
 
 function userSigninReducer(state={}, action) {
     switch(action.type) {
@@ -28,6 +28,19 @@ function userRegisterReducer(state={}, action) {
     }
 }
 
-export {
-    userSigninReducer, userRegisterReducer
+function userUpdateReducer(state = {}, action) {
+    switch (action.type) {
+      case USER_UPDATE_REQUEST:
+        return { loading: true };
+      case USER_UPDATE_SUCCESS:
+        return { loading: false, userInfo: action.payload };
+      case USER_UPDATE_FAIL:
+        return { loading: false, error: action.payload };
+      default: return state;
+    }
 }
+
+export {
+    userSigninReducer, userRegisterReducer, userUpdateReducer,
+}
+
--- Project-Padmahastha/frontend/src/store.js
+++ Project-Padmahastha/frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import Cookie from 'js-cookie';
 import { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
-import { userSigninReducer, userRegisterReducer } from './reducers/userReducers';
+import { userSigninReducer, userRegisterReducer, userUpdateReducer } from './reducers/userReducers';
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListReducer, orderDeleteReducer } from './reducers/orderReducers';
 
 const cartItems = Cookie.getJSON("cartItems") || [];
@@ -23,6 +23,7 @@ const reducer = combineReducers({
     orderPay: orderPayReducer,
     orderList: orderListReducer,
     orderDelete: orderDeleteReducer,
+    userUpdate: userUpdateReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
