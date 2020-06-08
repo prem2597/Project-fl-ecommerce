ca0402004695e96408172dd09888d144188efe48
Prem Kumar <prembammidi25@msitprogram.net>
2020-06-08 21:39:03 +0530

    admin profile

--- Project-Padmahastha/frontend/src/App.js
+++ Project-Padmahastha/frontend/src/App.js
@@ -46,6 +46,17 @@ function App() {
                 <Link to="/signin">Sign In</Link>
 
             }
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#">Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </header>
         <aside className="sidebar">
