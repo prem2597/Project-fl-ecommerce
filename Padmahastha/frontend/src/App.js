2ab305ce3be8425a448af48d1c3ee0d0c8aac284
unknown <Srii1020>
2020-06-08 13:50:05 +0530

    added route to oders

--- Padmahastha/frontend/src/App.js
+++ Padmahastha/frontend/src/App.js
@@ -1,3 +1,8 @@
+/**
+ * This is tha file which is in reactjs
+ * It's functionality is to display the header,footer and also all the routes are contained in this file.
+ * 
+ */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
@@ -11,15 +16,21 @@ import ProductsScreen from './Screens/ProductsScreen';
 import ShippingScreen from './Screens/ShippingScreen';
 import PaymentScreen from './Screens/PaymentScreen';
 import PlaceOrderScreen from './Screens/PlaceOrderScreen';
+import OrderScreen from './Screens/orderScreen';
 
 function App() {
 
 	const userSignin = useSelector(state => state.userSignin);
   	const { userInfo } = userSignin;
-
+    /**
+	 * THis is the function whihc is used to open the Hamburger menu
+	 */
   	const openMenu = () => {
     	document.querySelector(".sidebar").classList.add("open");
-  	}
+	  }
+	  /**
+	 * THis is the function whihc is used to close the Hamburger menu
+	 */
   	const closeMenu = () => {
     	document.querySelector(".sidebar").classList.remove("open")
   	}
@@ -31,7 +42,7 @@ function App() {
             			<button onClick={openMenu}>
               				&#9776;
         				</button>
-            			<Link to="/" >amazona</Link>
+            			<Link to="/" >Padmahastha</Link>
           			</div>
           			<div className="header-links">
             			<a href="cart.html">Cart</a>
@@ -51,6 +62,7 @@ function App() {
         		</aside>
         		<main className="main">
           			<div className="content">
+					  <Route path="/order/:id" component={OrderScreen} />
 						<Route path="/shipping" component={ShippingScreen} />
 						<Route path="/payment" component={PaymentScreen} />
 						<Route path="/placeorder" component={PlaceOrderScreen} />
