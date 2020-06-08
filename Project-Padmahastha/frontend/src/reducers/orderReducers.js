830ccfa6138955bdcd7793def76ac890f3e28794
Prem Kumar <prembammidi25@msitprogram.net>
2020-06-08 12:31:27 +0530

    order page backend

--- Project-Padmahastha/frontend/src/reducers/orderReducers.js
+++ Project-Padmahastha/frontend/src/reducers/orderReducers.js
@@ -5,7 +5,7 @@ function OrderCreateReducer(state = {}, action) {
         case ORDER_CREATE_REQUEST:
             return { loading: true };
         case ORDER_CREATE_SUCCESS:
-            return { loading: false, order: action.payload };
+            return { loading: false, order: action.payload, success: true };
         case ORDER_CREATE_FAIL:
             return { loading: false, error: action.payload };
         default:
