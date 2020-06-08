ed75ca93c0ec415dccde42ae121e1d0fa620d026
Prem Kumar <prembammidi25@msitprogram.net>
2020-06-08 17:59:06 +0530

    paypal complete integration

--- Project-Padmahastha/frontend/src/screens/orderScreen.js
+++ Project-Padmahastha/frontend/src/screens/orderScreen.js
@@ -8,12 +8,20 @@ import PaypalButton from '../components/PaypalButton'
 
 function OrderScreen(props) {
 
+    const orderPay = useSelector(state => state.orderPay);
+    const { loading: loadingPay, success: successPay, error: errorPay } = orderPay;
+    
     const dispatch = useDispatch();
+
     useEffect(() => {
-      dispatch(detailsOrder(props.match.params.id));
-      return () => {
-      };
-    }, [dispatch, props.match.params.id]);
+        if (successPay) {
+            props.history.push("/profile");
+        } else {
+            dispatch(detailsOrder(props.match.params.id));
+        }
+        return () => {
+        };
+    }, [dispatch, props.match.params.id, successPay]);
 
     const handleSuccessPayment = (paymentResult) => {
         dispatch(payOrder(order, paymentResult));
