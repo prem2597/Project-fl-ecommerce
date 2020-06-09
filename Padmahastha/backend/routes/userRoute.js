8003056a80e4a0310a898c29c1dd2da62f794aae
unknown <Srii1020>
2020-06-09 16:46:08 +0530

    added user profile route

--- Padmahastha/backend/routes/userRoute.js
+++ Padmahastha/backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import User from '../models/userModel';
-import { getToken } from '../util';
+import { getToken, isAuth } from '../util';
 
 const router = express.Router();
 /**
@@ -11,6 +11,28 @@ const router = express.Router();
  * if @isAdmin is true
  * if @status is 401 then the creadentails are incorrect
  */
+
+
+router.put('/:id', isAuth, async (req, res) => {
+	const userId = req.params.id;
+	const user = await User.findById(userId);
+	if (user) {
+	  user.name = req.body.name || user.name;
+	  user.email = req.body.email || user.email;
+	  user.password = req.body.password || user.password;
+	  const updatedUser = await user.save();
+	  res.send({
+		_id: updatedUser.id,
+		name: updatedUser.name,
+		email: updatedUser.email,
+		isAdmin: updatedUser.isAdmin,
+		token: getToken(updatedUser)
+	  });
+	} else {
+	  res.status(404).send({ msg: 'User Not Found' });
+	}
+  
+  }); 
 router.post('/signin', async (req, res) => {
     const signinUser = await User.findOne({
       	email: req.body.email,
